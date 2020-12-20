c930902da9bfc0b88f0735d430b23bd4a3ba4619
Евгений <catshaurma@gmail.com>
2020-12-20 22:16:28 +0300

    Add to
    Create BaseTo and UserTo classes

---
+++ src/main/java/to/BaseTo.java
@@ -0,0 +1,24 @@
+package to;
+
+public abstract class BaseTo {
+    protected Integer id;
+
+    public BaseTo() {
+    }
+
+    public BaseTo(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
